18751de7c8ccd4cef0987e4ea876818d074ef075
Tosin-Mathew <emmanueltosinmathew@gmail.com>
2025-09-20 11:35:42 +0100

    Create players.js

---
+++ players.js
@@ -0,0 +1,44 @@
+const players = [
+  {
+    name: "Lionel Messi",
+    team: "Inter Miami CF",
+    nationality: "Argentinian",
+    jerseyNumber: 10,
+    age: 36,
+    imageUrl: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt12dbddde5342ce4c/60d866a69c6af30f4b050c8d/8005f7a2f385d4c9e4c2c15b9f2b3a8c9b4c0a2f.jpg"
+  },
+  {
+    name: "Cristiano Ronaldo",
+    team: "Al Nassr",
+    nationality: "Portuguese",
+    jerseyNumber: 7,
+    age: 39,
+    imageUrl: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt62e31f8d0c58b34b/60d866a6c9b2c30f4b050c8e/8005f7a2f385d4c9e4c2c15b9f2b3a8c9b4c0a2f.jpg"
+  },
+  {
+    name: "Kylian Mbappé",
+    team: "Paris Saint-Germain",
+    nationality: "French",
+    jerseyNumber: 7,
+    age: 25,
+    imageUrl: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt62e31f8d0c58b34b/60d866a6c9b2c30f4b050c8e/8005f7a2f385d4c9e4c2c15b9f2b3a8c9b4c0a2f.jpg"
+  },
+  {
+    name: "Erling Haaland",
+    team: "Manchester City",
+    nationality: "Norwegian",
+    jerseyNumber: 9,
+    age: 23,
+    imageUrl: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt62e31f8d0c58b34b/60d866a6c9b2c30f4b050c8e/8005f7a2f385d4c9e4c2c15b9f2b3a8c9b4c0a2f.jpg"
+  },
+  {
+    name: "Kevin De Bruyne",
+    team: "Manchester City",
+    nationality: "Belgian",
+    jerseyNumber: 17,
+    age: 32,
+    imageUrl: "https://assets.goal.com/v3/assets/bltcc7a7ffd2fbf71f5/blt62e31f8d0c58b34b/60d866a6c9b2c30f4b050c8e/8005f7a2f385d4c9e4c2c15b9f2b3a8c9b4c0a2f.jpg"
+  }
+];
+
+export default players;
